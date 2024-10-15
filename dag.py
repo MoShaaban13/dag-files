eba4092af5a97f3219a93a7276b4e99d41375e18
MoAshraf259 <m7mda4f.ma@gmail.com>
2024-10-16 01:37:08 +0300

    Update DAG to include resources fix2

--- dag.py
+++ dag.py
@@ -1,22 +1,27 @@
 from airflow import DAG
-from dags.operator.custom_kubernetes_pod_operator import CustomKubernetesPodOperator
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 from airflow.utils.dates import days_ago
 
 with DAG(
-    dag_id="simple_custom_print_dag",
+    dag_id="simple_print_dag",
     start_date=days_ago(1),
     schedule_interval=None,
     catchup=False,
 ) as dag:
 
-    print_task = CustomKubernetesPodOperator(
+    print_task = KubernetesPodOperator(
         task_id="print_task",
+        name="print-pod",
         namespace="default",  # Set your namespace
         image="ubuntu",  # Use a lightweight image
-        cmds=["bash", "-c"],
-        arguments=["echo 'Hello from Custom Kubernetes Pod!'"],  # Command to execute
-        resources={'request_memory': '256Mi', 'limit_memory': '256Mi', 'request_cpu': '0.5', 'limit_cpu': '0.5'},
-        is_delete_operator_pod=True,  # Cleanup the pod after execution
+        cmds=["bash", "-c"],  # Use bash to execute commands
+        arguments=["echo 'Hello from Kubernetes Pod!'"],  # The command to execute
+        resources={
+            "request_memory": "256Mi",  # Request memory
+            "limit_memory": "256Mi",  # Limit memory
+            "request_cpu": "500m",  # Request CPU in millicores
+            "limit_cpu": "500m",  # Limit CPU in millicores
+        },
         dag=dag
     )
 
