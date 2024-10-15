ecacd8c9aa181cddc698538aee078d136bff32c8
MoAshraf259 <m7mda4f.ma@gmail.com>
2024-10-16 01:12:00 +0300

    Update DAG to include resources fix

--- dag.py
+++ dag.py
@@ -19,10 +19,14 @@ with DAG(
         arguments=["echo", "Hello World"],
         execution_timeout=timedelta(minutes=10),  # Set timeout to 10 minutes
         resources={
-            "request_cpu": "0.5",  # Requesting half a core
-            "request_memory": "500Mi",  # Requesting 0.5 GB of memory
-            "limit_cpu": "0.5",  # Limiting to half a core
-            "limit_memory": "500Mi",  # Limiting to 0.5 GB of memory
-        }
+            "limits": {
+                "cpu": "0.5",  # Limiting to half a core
+                "memory": "500Mi",  # Limiting to 0.5 GB of memory
+            },
+            "requests": {
+                "cpu": "0.5",  # Requesting half a core
+                "memory": "500Mi",  # Requesting 0.5 GB of memory
+            },
+        },
     )
 
