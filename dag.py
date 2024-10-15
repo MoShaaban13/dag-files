bce2ac5872ef499d2039b1a671b086d3e3ab708d
MoAshraf259 <m7mda4f.ma@gmail.com>
2024-10-16 01:29:52 +0300

    Update DAG to include resources fix2

--- dag.py
+++ dag.py
@@ -1,28 +1,28 @@
 from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 from airflow.utils.dates import days_ago
-from datetime import timedelta
 
 with DAG(
-    dag_id="dag_with_pod_operator",
+    dag_id="simple_print_dag",
     start_date=days_ago(1),
     schedule_interval=None,
     catchup=False,
 ) as dag:
 
-    task_create_pod = KubernetesPodOperator(
-        task_id="create_pod",
-        name="example-pod",
-        namespace="default",
-        image="ubuntu",
-        cmds=["bash", "-cx"],
-        arguments=["echo", "Hello World"],
-        execution_timeout=timedelta(minutes=10),  # Set timeout to 10 minutes
+    print_task = KubernetesPodOperator(
+        task_id="print_task",
+        name="print-pod",
+        namespace="default",  # Set your namespace
+        image="ubuntu",  # Use a lightweight image
+        cmds=["bash", "-c"],  # Use bash to execute commands
+        arguments=["echo 'Hello from Kubernetes Pod!'"],  # The command to execute
         resources={
-            "request_cpu": "0.5",  # Requesting half a core
-            "request_memory": "500Mi",  # Requesting 0.5 GB of memory
-            "limit_cpu": "0.5",  # Limiting to half a core
-            "limit_memory": "500Mi",  # Limiting to 0.5 GB of memory
-        }
+            "request_memory": "256Mi",  # Request memory
+            "limit_memory": "256Mi",  # Limit memory
+            "request_cpu": "0.5",  # Request CPU
+            "limit_cpu": "0.5",  # Limit CPU
+        },
+        is_delete_operator_pod=True,  # Cleanup the pod after execution
+        dag=dag
     )
 
