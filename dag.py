4d4deba2d693287003c12925b9db6ea2dce85586
Mohamed Ashraf Shaaban <143937148+MoShaaban13@users.noreply.github.com>
2024-10-16 10:18:28 +0300

    Update dag.py

--- dag.py
+++ dag.py
@@ -1,22 +1,39 @@
 from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 from airflow.utils.dates import days_ago
+from datetime import timedelta
 
-with DAG(
-    dag_id="simple_print_dag",
-    start_date=days_ago(1),
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': days_ago(1),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+# Define the DAG
+dag = DAG(
+    dag_id='pod_stay_alive_5_minutes',
+    default_args=default_args,
+    description='A DAG to run a pod that stays alive for at least 5 minutes',
     schedule_interval=None,
     catchup=False,
-) as dag:
+)
 
-    print_task = KubernetesPodOperator(
-        task_id="print_task",
-        name="print-pod",
-        namespace="default",  # Set your namespace
-        image="ubuntu",  # Use a lightweight image
-        cmds=["bash", "-c"],  # Use bash to execute commands
-        arguments=["echo 'Hello from Kubernetes Pod!'"],  # The command to execute
-        is_delete_operator_pod=True,  # Cleanup the pod after execution
-        dag=dag
-    )
+# Define the KubernetesPodOperator task
+stay_alive_pod = KubernetesPodOperator(
+    namespace='default',
+    image="ubuntu:latest",  # Using a lightweight Ubuntu image
+    cmds=["bash", "-c"],  # Run bash commands
+    arguments=["echo 'Pod will stay alive for 5 minutes' && sleep 300"],  # Sleep for 300 seconds (5 minutes)
+    labels={"purpose": "stay-alive"},
+    name="stay-alive-pod",
+    task_id="stay_alive_pod_task",
+    get_logs=True,  # Retrieve logs from the pod
+    is_delete_operator_pod=True,  # Delete the pod after it finishes
+    dag=dag,
+)
 
+# If there are other tasks, define dependencies here
