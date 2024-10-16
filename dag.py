6b2c9c5f6b744a91779fcee16a5c1346de940fe6
Mohamed Ashraf Shaaban <143937148+MoShaaban13@users.noreply.github.com>
2024-10-16 10:41:56 +0300

    Update dag.py

--- dag.py
+++ dag.py
@@ -14,9 +14,9 @@ default_args = {
 }
 
 dag = DAG(
-    dag_id='pod_stay_alive_ubuntu_5_minutes_with_resources',
+    dag_id='pod_stay_alive_ubuntu_5_minutes',
     default_args=default_args,
-    description='A DAG to run a pod with ubuntu image that stays alive for at least 5 minutes and has resource requests and limits',
+    description='A DAG to run a pod with ubuntu image that stays alive for at least 5 minutes',
     schedule_interval=None,
     catchup=False,
 )
@@ -32,12 +32,6 @@ stay_alive_pod = KubernetesPodOperator(
     task_id="stay_alive_pod_task_ubuntu",
     get_logs=True,  # Retrieve logs from the pod
     is_delete_operator_pod=True,  # Delete the pod after completion
-    resources={
-        "limit_memory": "512Mi",
-        "limit_cpu": "500m",
-        "request_memory": "256Mi",
-        "request_cpu": "200m",
-    },  # Resource requests and limits in the correct format
     dag=dag,
 )
 
